b3c718bd04e2520bd9ab6c83f7afc6f0c1015079
Tommy Bishop <tommy@tier-9.com>
2009-07-04 08:17:44 +0000

    moved the logic that separates line numbers and code into the model.  could still use some refactoring.

--- app/models/paste.rb
+++ app/models/paste.rb
@@ -1,3 +1,38 @@
 class Paste < ActiveRecord::Base
   belongs_to :parser
+  
+  def line_numbers
+    parsed_output = Uv.parse(self.code, 'xhtml', self.parser.name, true, 'twilight')
+    doc = Nokogiri::HTML(parsed_output)
+    
+    lin_num = ""
+    
+    doc.xpath('//pre').each do |root|
+      root.children.each do |n|
+        if n['class'] == 'line-numbers'
+          lin_num += n.content + "\n"
+        end
+      end
+    end
+    
+    return lin_num    		  
+  end
+  
+  def highlighted_code
+    parsed_output = Uv.parse(self.code, 'xhtml', self.parser.name, true, 'twilight')
+    doc = Nokogiri::HTML(parsed_output)
+    
+    hcode = ""
+    
+    doc.xpath('//pre').each do |root|
+      root.children.each do |n|
+        if n['class'] != 'line-numbers'
+          hcode += n.to_s
+        end
+      end
+    end
+    
+    return code
+  end
+  
 end
