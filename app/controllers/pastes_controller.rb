a24c730a0a08ce9f92f8176df6c3f2a7ae968895
Tommy Bishop <tommy@tier-9.com>
2009-07-05 11:36:54 +0000

    added header nav links and css.  also added support for raw file (.txt).

--- app/controllers/pastes_controller.rb
+++ app/controllers/pastes_controller.rb
@@ -18,6 +18,7 @@ class PastesController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @paste }
+      format.text { render :text => @paste.code }
     end
   end
 
