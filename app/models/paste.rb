34dfd826a77194ffde07e8b7c87b4103090563c1
Tommy Bishop <tommy@tier-9.com>
2009-07-05 07:26:00 +0000

    fixed highlighted text (now properly includes span tags).  also update views and stylesheets to make sure highlighted text is display correctly.

--- app/models/paste.rb
+++ app/models/paste.rb
@@ -5,15 +5,18 @@ class Paste < ActiveRecord::Base
     parsed_output = Uv.parse(self.code, 'xhtml', self.parser.name, true, 'twilight')
     doc = Nokogiri::HTML(parsed_output)
     
-    lin_num = ""
+    # lin_num = "<pre id=\"source-numbers\" class=\"twilight\"><span class=\"Comment\">"
+    lin_num = ''
     
     doc.xpath('//pre').each do |root|
       root.children.each do |n|
         if n['class'] == 'line-numbers'
-          lin_num += n.content + "\n"
+          lin_num += n.content.strip + "\n"
         end
       end
     end
+    
+    # lin_num += "</span></pre>"
     
     return lin_num    		  
   end
@@ -25,14 +28,18 @@ class Paste < ActiveRecord::Base
     hcode = ""
     
     doc.xpath('//pre').each do |root|
+      if root['class'] == 'twilight'
+        root['id'] = 'source-code'
+      end
+      
       root.children.each do |n|
-        if n['class'] != 'line-numbers'
-          hcode += n.to_s
+        if n['class'] == 'line-numbers'
+          n.unlink
         end
       end
     end
     
-    return code
+    return doc
   end
   
 end
