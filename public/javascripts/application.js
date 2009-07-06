8653a954a35cb025f42df788abffdc503208dc0d
Tommy Bishop <tommy@tier-9.com>
2009-07-06 05:23:44 +0000

    refactored theme switching js.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,30 +1,14 @@
 // this will set our theme as desired
 function set_theme(new_theme) {
-	var classArray = $('code').classNames().toArray();
-
-    for (var index = 0, len = classArray.size(); index < len; ++index) {
-        $('code').removeClassName(classArray[index]);
+	var theme_divs = ['code', 'source-numbers', 'source-code'];
+	
+	for (tdiv in theme_divs) {
+		var classArray = $(theme_divs[tdiv]).classNames().toArray();
+		
+		for (var index = 0, len = classArray.size(); index < len; ++index) {
+	        	$(theme_divs[tdiv]).removeClassName(classArray[index]);
+		}
+		
+		Element.addClassName(theme_divs[tdiv], new_theme);
 	}
-	
-	Element.addClassName('code', new_theme);
-	
-	
-	
-	var classArray = $('source-numbers').classNames().toArray();
-
-    for (var index = 0, len = classArray.size(); index < len; ++index) {
-        $('source-numbers').removeClassName(classArray[index]);
-	}
-	
-	Element.addClassName('source-numbers', new_theme);
-	
-	
-	
-	var classArray = $('source-code').classNames().toArray();
-
-    for (var index = 0, len = classArray.size(); index < len; ++index) {
-        $('source-code').removeClassName(classArray[index]);
-	}
-	
-	Element.addClassName('source-code', new_theme);
 }
