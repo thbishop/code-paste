82fc1866749b166d7680bda7fb37fb4a3c076dfa
Tommy Bishop <tommy@tier-9.com>
2009-08-07 05:18:25 +0000

    removed unnecessary code.

--- test/factories.rb
+++ test/factories.rb
@@ -9,18 +9,12 @@ end
 
 Factory.define :rails_snip, :class => 'paste' do |snip|  
   snip.code "class Paste < ActiveRecord::Base\r\n  belongs_to :parser\r\n  before_validation :calc_num_of_lines\r\n  \r\n  def line_numbers\r\n    doc = get_parsed_code\r\n    \r\n    lin_num = ''\r\n    \r\n    doc.xpath('//pre').each do |root|\r\n      root.children.each do |n|\r\n        if n['class'] == 'line-numbers'\r\n          lin_num += n.content.strip + \"\\n\"\r\n        end\r\n      end\r\n    end\r\n    \r\n    return lin_num    \t\t  \r\n  end\r\n  \r\n  def highlighted_code\r\n    doc = get_parsed_code\r\n    \r\n    hcode = \"\"\r\n    \r\n    doc.xpath('//pre').each do |root|\r\n      if root['class'] == 'twilight'\r\n        root['id'] = 'source-code'\r\n      end\r\n      \r\n      root.children.each do |n|\r\n        if n['class'] == 'line-numbers'\r\n          n.unlink\r\n        end\r\n      end\r\n    end\r\n    \r\n    return doc\r\n  end\r\n  \r\n  private\r\n  def get_parsed_code\r\n    parsed_output = Uv.parse(self.code, 'xhtml', self.parser.name, true, 'blackboard')\r\n    doc = Nokogiri::HTML(parsed_output)    \r\n  end\r\n  \r\n  def calc_num_of_lines\r\n    doc = get_parsed_code\r\n    \r\n    lin_num = ''\r\n    \r\n    doc.xpath('//pre').each do |root|\r\n      root.children.each do |n|\r\n        if n['class'] == 'line-numbers'\r\n          lin_num = ''\r\n          lin_num += n.content.strip\r\n        end\r\n      end\r\n    end\r\n    \r\n    self.number_of_lines = lin_num.to_i\r\n  end\r\n  \r\nend\r\n"
-  # p.association :parser, :factory => :rails_parser
   snip.parser {|p| p.association :parser, :name => 'ruby_on_rails', :display_name => 'Ruby on Rails' }
 end
 
 Factory.define :xml_snip, :class => 'paste' do |snip|
-  # def snip.default_parent
-  #     @default_parent ||= Factory(:parser, :name => 'xml', :display_name => 'XML')
-  # end
   snip.code "<note>\r\n <to>Tov</to>\r\n  <from>Jani</from>\r\n <heading>Reminder</heading>\r\n <body>Don't forget me this weekend!</body>\r\n  </note>"
-  # snip.parser {|p| p.association :xml_parser }
   snip.parser {|p| p.association :parser, :name => 'xml', :display_name => 'XML' }
-  # snip.parser_id { snip.default_parent.id }
 end
 
 Factory.define :perl_snip, :class => 'paste' do |snip|
